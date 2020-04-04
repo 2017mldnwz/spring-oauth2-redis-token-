34c95d76d111cc9d285dc063c8be69c160a004c6
2017mldnwz <295143473@qq.com>
2020-04-04 21:33:24 +0800

    1

---
+++ src/main/java/com/dy/client/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.dy.client.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Order(1)
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+	
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+		http.formLogin() // 表单方式
+        .and()
+        .authorizeRequests() // 授权配置
+        .anyRequest()  // 所有请求
+        .authenticated(); // 都需要认证
+    }
+}
