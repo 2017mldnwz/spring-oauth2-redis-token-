944d8c190dccaf9cc73167d4860c689a5c40fd93
2017mldnwz <295143473@qq.com>
2020-04-05 10:49:17 +0800

    1

--- src/main/java/com/dy/client/controller/UserController.java
+++ src/main/java/com/dy/client/controller/UserController.java
@@ -1,16 +1,7 @@
 package com.dy.client.controller;
 
-import java.nio.charset.StandardCharsets;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import io.jsonwebtoken.Jwts;
 
 @RestController
 public class UserController {
