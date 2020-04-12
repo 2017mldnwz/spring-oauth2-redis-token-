98fc086b0eb5d2a1ad0de09ab71be4d9916945af
2017mldnwz <295143473@qq.com>
2020-04-12 17:00:51 +0800

    1

--- src/main/java/com/dy/client/config/SecurityConfig.java
+++ src/main/java/com/dy/client/config/SecurityConfig.java
@@ -1,17 +1,28 @@
 package com.dy.client.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.dy.client.service.impl.UserDetailService;
 
 @Order(1)
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private UserDetailService userDetailService;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	@Bean(name = BeanIds.AUTHENTICATION_MANAGER)
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
@@ -26,4 +37,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .anyRequest()  // 所有请求
         .authenticated(); // 都需要认证
     }
+	
+	@Bean  
+    public AuthenticationProvider authenticationProvider() {  
+		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+		authenticationProvider.setUserDetailsService(userDetailService);
+		authenticationProvider.setPasswordEncoder(passwordEncoder); // 设置密码加密方式
+        return authenticationProvider;  
+    }  
 }
